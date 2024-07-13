21fc2ae473c90dd5d43cb612274ec49534bca81d
anastasiia-natina <132943209+anastasiia-natina@users.noreply.github.com>
2024-07-13 19:33:03 +0200

    init

---
+++ task_1.py
@@ -0,0 +1,71 @@
+import asyncio
+import argparse
+import logging
+import os
+from pathlib import Path
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("source_folder", help="Шлях до вихідної папки")
+parser.add_argument("output_folder", help="Шлях до цільової папки")
+args = parser.parse_args()
+
+source_folder = Path(args.source_folder)
+output_folder = Path(args.output_folder)
+
+if not source_folder.exists():
+    logging.error(f"Вихідна папка {source_folder} не існує.")
+    exit(1)
+
+if not output_folder.exists():
+    try:
+        output_folder.mkdir()
+    except OSError as e:
+        logging.error(f"Не вдалося створити цільову папку {output_folder}: {e}")
+        exit(1)
+
+async_source_folder = os.path.join(os.path.realpath(source_folder), "")
+async_output_folder = os.path.join(os.path.realpath(output_folder), "")
+
+
+async def read_folder(folder):
+    for entry in os.scandir(folder):
+        if entry.is_dir():
+            await read_folder(os.path.join(folder, entry.name))
+        else:
+            await copy_file(entry.name, folder)
+
+
+async def copy_file(filename, folder):
+    file_extension = os.path.splitext(filename)[1]
+    output_subfolder = os.path.join(async_output_folder, file_extension[1:])
+
+    if not os.path.exists(output_subfolder):
+        try:
+            os.makedirs(output_subfolder)
+        except OSError as e:
+            logging.error(f"Не вдалося створити підпапку {output_subfolder}: {e}")
+            return
+
+    source_file = os.path.join(folder, filename)
+    destination_file = os.path.join(output_subfolder, filename)
+
+    try:
+        await asyncio.copyfile(source_file, destination_file)
+        logging.info(f"Скопійовано файл {filename} до {destination_file}")
+    except Exception as e:
+        logging.error(f"Помилка при копіюванні файлу {filename}: {e}")
+
+
+async def main():
+    try:
+        await asyncio.gather(read_folder(async_source_folder))
+    except KeyboardInterrupt:
+        logging.info("Переривання...")
+
+
+if __name__ == "__main__":
+    logging.info(f"Початок сортування файлів з {source_folder} до {output_folder}")
+    asyncio.run(main())
+    logging.info("Сортування файлів завершено.")
---
+++ task_2.py
@@ -0,0 +1,54 @@
+import asyncio
+import matplotlib.pyplot as plt
+import requests
+from collections import Counter
+from typing import Dict
+
+async def download_text(url):
+    async with requests.get(url) as response:
+        if response.status_code == 200:
+            return await response.text
+        else:
+            raise Exception(f"Помилка завантаження тексту: {response.status_code}")
+
+
+
+def map_reduce_word_frequency(text):
+
+    def mapper(word):
+        return word.lower(), 1
+
+    def reducer(word, counts):
+        yield word, sum(counts)
+
+    words = text.split()
+    word_counts = Counter(map(mapper, words))
+    return dict(reducer(word, word_counts[word]) for word in word_counts)
+
+def visualize_top_words(word_counts, top_n=10):
+    top_words = sorted(word_counts.items(), key=lambda item: item[1], reverse=True)[:top_n]
+    labels, values = zip(*top_words)
+
+    plt.figure(figsize=(8, 6))
+    plt.bar(labels, values)
+    plt.xlabel("Слово")
+    plt.ylabel("Частота")
+    plt.title(f"Топ-{top_n} найчастіше вживаних слів")
+    plt.xticks(rotation=45, ha="right")
+    plt.tight_layout()
+    plt.show()
+
+
+async def main():
+    url = "https://gutenberg.net.au/ebooks01/0100021.txt" 
+
+    try:
+        text = await download_text(url)
+        word_counts = map_reduce_word_frequency(text)
+        visualize_top_words(word_counts)
+    except Exception as e:
+        print(f"Помилка: {e}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
